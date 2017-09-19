6b034927a5b0311f5aec9001baad14768bceebd6
ZEHUA WANG <15122302157@163.com>
2017-09-19 12:18:24 -0400

    Update DragonTest.java

--- AssignMent1/DragonTest.java
+++ AssignMent1/DragonTest.java
@@ -38,30 +38,33 @@ import com.jme3.system.AppSettings;
 /**
  * @version 1.0.0
  * @author Zehua Wang
+ * @Usage: To change the view of the camera hold left mouse and turn
  */
 public class DragonTest extends SimpleApplication {
     public static void main(String[] args){
     DragonTest app = new DragonTest();
     app.start();
     }
+    //set the variable we need to use
     protected Spatial grog;
     private ChaseCamera chaseCam;
     Boolean isRunning = true;   
     @Override
     public void simpleInitApp() {
+    //disable the default First Person Camera    
     flyCam.setEnabled(false);
     flyCam.setMoveSpeed(30);
-//  chaseCam = new ChaseCamera(cam, grog, inputManager);
-//  chaseCam.setSmoothMotion(true);
+    //import a material 
+    Material mat_default = new Material(assetManager,"Common/MatDefs/Misc/ShowNormals.j3md");
+    //import the grog model and set the scale and location
     grog = assetManager.loadModel("Models/grog5k/grog5k.j3o");
     rootNode.attachChild(grog);
-    Material mat_default = new Material(assetManager,"Common/MatDefs/Misc/ShowNormals.j3md");
     grog.setMaterial(mat_default);
     grog.setLocalTranslation(0f,8.9f,0f);
     grog.scale(0.25f,0.25f,0.25f);
+    //create a new chase camera to the grog and bundle it to grog
     chaseCam = new ChaseCamera(cam, grog, inputManager);
     chaseCam.setSmoothMotion(true);
-    initKeys();//load my custom keybiding
     //add the HighField Scene map
     Spatial Scene = assetManager.loadModel("Scenes/HeightMap.j3o");
     rootNode.attachChild(Scene);
@@ -69,7 +72,9 @@ public class DragonTest extends SimpleApplication {
     sun.setDirection(new Vector3f(-0.0f,-0.0f,-0.0f));
     sun.setColor(ColorRGBA.White);
     rootNode.addLight(sun);
-    //add the triceratops on the corner
+    //load my custom keybiding
+    initKeys();
+    //add the triceratops on the corner and set the material
     Spatial trice_1 = assetManager.loadModel("Models/triceratops/triceratops.j3o");
     Spatial trice_2 = assetManager.loadModel("Models/triceratops/triceratops.j3o");
     Spatial trice_3 = assetManager.loadModel("Models/triceratops/triceratops.j3o");
@@ -78,19 +83,15 @@ public class DragonTest extends SimpleApplication {
     trice_2.setMaterial(mat_default);
     trice_3.setMaterial(mat_default);
     trice_4.setMaterial(mat_default);
-    
+    //put the triceratops at the corner of the wall
     trice_1.setLocalScale(0.25f,0.25f,0.25f);
     trice_1.move(60.0f,14.5f,40.0f);
-    
     trice_2.setLocalScale(0.25f,0.25f,0.25f);
     trice_2.move(65.f,14.5f,-50.0f);
-    
     trice_3.setLocalScale(0.25f,0.25f,0.25f);
     trice_3.move(-30.0f,14.5f,40.0f);
-    
     trice_4.setLocalScale(0.25f,0.25f,0.25f);
     trice_4.move(-30.0f,14.5f,-60.0f);
-    
     rootNode.attachChild(trice_1);
     rootNode.attachChild(trice_2);
     rootNode.attachChild(trice_3);
@@ -108,28 +109,29 @@ public class DragonTest extends SimpleApplication {
     private AnalogListener analogListener = new AnalogListener() {
     public void onAnalog(String name, float value, float tpf) {
       if (isRunning) {
-//        direction.set(cam.getDirection()).normalizeLocal();
         if (name.equals("Right")) {
-//        Vector3f v = grog.getLocalTranslation();
-//        grog.setLocalTranslation(v.x + value*speed*10, v.y, v.z);
-          grog.move(5*tpf,0,0);  
+//      Vector3f v = grog.getLocalTranslation();
+//      grog.setLocalTranslation(v.x + value*speed*10, v.y, v.z);
+        grog.move(5*tpf,0,0);  
         }
         if (name.equals("Left")) {
-//        Vector3f   v = grog.getLocalTranslation();
-//        grog.setLocalTranslation(v.x - value*speed*10, v.y, v.z);
+//      Vector3f   v = grog.getLocalTranslation();
+//      grog.setLocalTranslation(v.x - value*speed*10, v.y, v.z);
         grog.move(-5 * tpf, 0, 0);
         }
         if (name.equals("Front")) {
-//        Vector3f   v = grog.getLocalTranslation();
-//        grog.setLocalTranslation(v.x , v.y, v.z+ value*speed*10);
+//      Vector3f   v = grog.getLocalTranslation();
+//      grog.setLocalTranslation(v.x , v.y, v.z+ value*speed*10);
         grog.move(0, 0, -5 * tpf);
         }
         if (name.equals("Back")) {
-//        Vector3f  v = grog.getLocalTranslation();
-//        grog.setLocalTranslation(v.x , v.y, v.z- value*speed*10);
+//      Vector3f  v = grog.getLocalTranslation();
+//      grog.setLocalTranslation(v.x , v.y, v.z- value*speed*10);
             grog.move(0, 0, 5 * tpf);
         }
       } 
     }
   };
 }
+    
+
